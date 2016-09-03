39b1e2108851dec5b613db7161725e39485848db
Admin <Admin>
2016-09-03 13:54:08 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+
+    public static void main(String[] args) {
+        int tmp=0;
+        for (int i=0;i<500;i++){
+            tmp = tmp+i;
+
+        }
+
+        System.out.println("Hello World!"+tmp);
+    }
+}
